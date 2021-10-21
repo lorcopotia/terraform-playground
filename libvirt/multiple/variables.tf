b9769958283e62b9ed778758767b97d1b5264ea9
Duanel Garrido Milan <lorcopotia@gmail.com>
2021-10-21 12:52:05 +0200

    Create variables.tf

---
+++ libvirt/multiple/variables.tf
@@ -0,0 +1,7 @@
+variable "hostname" {
+  type    = list(string)
+  default = ["dev1-centos", "dev2-centos"]
+}
+variable "domain" { default = "local" }
+variable "memoryMB" { default = 1024*2 }
+variable "cpu" { default = 2 }
