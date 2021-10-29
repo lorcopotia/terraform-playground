77491944c9df506f151ad958ee3c62fa75b2c992
Duanel Garrido Milan <lorcopotia@gmail.com>
2021-10-29 10:21:22 +0200

    Update centos.tf

--- libvirt/single-vm/centos.tf
+++ libvirt/single-vm/centos.tf
@@ -25,30 +25,10 @@ resource "libvirt_domain" "vm-centos" {
     network_name = "default"
   }
 
-  # IMPORTANT: this is a known bug on cloud images, since they expect a console
-  # we need to pass it
-  # https://bugs.launchpad.net/cloud-images/+bug/1573095
-  console {
-    type        = "pty"
-    target_port = "0"
-    target_type = "serial"
-  }
-
-  console {
-    type        = "pty"
-    target_type = "virtio"
-    target_port = "1"
-  }
-
   disk {
-    volume_id = libvirt_volume.ubuntu-qcow2.id
+    volume_id = libvirt_volume.centos-qcow2.id
   }
 
-  graphics {
-    type        = "spice"
-    listen_type = "address"
-    autoport    = true
-  }
 }
 
 # IPs: use wait_for_lease true or after creation use terraform refresh and terraform show for the ips of domain
