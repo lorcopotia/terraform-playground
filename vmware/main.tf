51ca41a9bb19ba3fc69800f910bad641d4d3f423
Duanel Garrido <lorcopotia@gmail.com>
2023-04-14 09:29:46 +0200

    Update: Added new provider

---
+++ vmware/main.tf
@@ -0,0 +1,60 @@
+data "vsphere_datacenter" "datacenter" {
+  name = var.datacenter
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = var.datastore
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = var.cluster
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_host" "host" {
+  name          = var.host
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_network" "network" {
+  name          = var.port-group
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = var.template
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = var.vm-name
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  host_system_id   = data.vsphere_host.host.id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  num_cpus         = 1
+  memory           = 1024
+  guest_id         = "centos7_64Guest"
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+  disk {
+    label = "disk0"
+    size  = 50
+  }
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+    customize {
+      linux_options {
+        host_name = "hello-world"
+        domain    = "es.logicalis.com"
+      }
+      network_interface {
+        ipv4_address = "172.16.11.10"
+        ipv4_netmask = 24
+      }
+      ipv4_gateway = "172.16.11.1"
+    }
+  }
+}
+
---
+++ vmware/variables.tf
@@ -0,0 +1,56 @@
+variable "vm-name" {
+  type        = string
+  description = "Name of the instance"
+}
+variable "template" {
+  type        = string
+  description = "Name of the template to be used for deployment"
+}
+variable "datacenter" {
+  type        = string
+  description = "Name of the Datacenter to be used for deployment"
+}
+variable "cluster" {
+  type        = string
+  description = "Name of the Cluster to be used for deployment"
+}
+variable "host" {
+  type        = string
+  description = "Name of the ESXi host to be used for deployment"
+}
+variable "folder" {
+  type        = string
+  description = "Name of the folder to be used for deployment"
+}
+variable "datastore" {
+  type        = string
+  description = "Name of the Datastore to be used for deployment"
+}
+variable "admin-passwd" {
+  type        = string
+  default     = "Sup3rSecr3t"
+  description = "Set an Admin passwd"
+}
+variable "quantity" {
+  type        = number
+  description = "How many instances to be deployed"
+}
+variable "port-group" {
+  type        = string
+  description = "Name of the Port Group to be used for deployment"
+}
+variable "vsphere-user" {
+  type        = string
+  description = "Set vSphere username for connection"
+  default     = "administrator@vsphere.local"
+}
+variable "vsphere-server" {
+  type        = string
+  description = "Set vSphere Server"
+  default     = "lgchfvc01.vcloud.morse.lab"
+}
+variable "vsphere-passwd" {
+  type        = string
+  description = "Set vSphere passwd for connection"
+  default     = "L0g1c4l1s2015*"
+}
