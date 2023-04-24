3b944af4054c3ca7a5ae14360270d55d13d1e93e
Duanel Garrido Milan <lorcopotia@gmail.com>
2023-04-24 17:56:25 +0200

    Update centos.tf

--- libvirt/single-vm/centos.tf
+++ libvirt/single-vm/centos.tf
@@ -14,6 +14,21 @@ resource "libvirt_volume" "centos-qcow2" {
   qemu_agent = true
 }
 
+# for more info about paramater check this out
+# https://github.com/dmacvicar/terraform-provider-libvirt/blob/master/website/docs/r/cloudinit.html.markdown
+# Use CloudInit to add our ssh-key to the instance
+# you can add also meta_data field
+resource "libvirt_cloudinit_disk" "commoninit" {
+  name           = "commoninit.iso"
+  user_data      = templatefile("${path.module}/cloud_init_centos_ansible.cfg", { hostname = "${var.hostname}", fqdn = "${var.hostname}.${var.domain}" })
+  network_config = data.template_file.network_config.rendered
+  pool           = "default"
+}
+
+data "template_file" "network_config" {
+  template = file("${path.module}/network_config.cfg")
+}
+
 # Create the machine
 resource "libvirt_domain" "vm-centos" {
   name   = var.hostname
